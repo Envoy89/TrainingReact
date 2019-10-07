e137d251458a2dbe6b6f6a97ef7cee29832a2843
Minyazev Ruzil <ruzil33@gmail.com>
2019-10-08 00:38:35 +0300

    remove unused register

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './store/configureStore'
 import App from './App'
 
-import registerServiceWorker from './registerServiceWorker'
+import { register } from './serviceWorker'
 
 import './index.css'
 
@@ -15,4 +15,4 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-registerServiceWorker()
+register()
